ec222aab0d73ddbe38e70cbd69e6c9078fa30d57
Ryunosuke Yokokawa <ryokokawa00mylangara.ca>
2022-02-18 15:40:26 -0800

    edit profile page

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import {Avatar} from "native-base";
+import {Avatar, Pressable} from "native-base";
+import { useState } from "react";
+import { Modal } from "react-native";
 import { Text, View } from 'react-native';
+import EditParentProfile from "./src/component/modal/editParentProfile";
 import ActivityScreen from "./src/component/screens/ActivityScreen";
 import RewardScreen from "./src/component/screens/RewardScreen";
 import TaskScreen from './src/component/screens/TaskScreen'
+import { NativeRouter, Route, Link } from "react-router-native";
+import EditChildProfile from "./src/component/modal/editChildProfile";
+
 
 
 function SettingsScreen() {
+  
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Text>Settings!</Text>
@@ -16,7 +23,14 @@ function SettingsScreen() {
 const Tab = createBottomTabNavigator()
 const uriForImg = "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
 
-const options = {
+
+export default function(){
+  const [editParent, setEditParent] = useState(true)
+
+  const [showModal, setShowModal] = useState(false)
+
+
+  const options = {
     headerStyle : {
         height : 110,
       },
@@ -25,21 +39,29 @@ const options = {
       },
       headerRight : ()=>{
         return (
-          <Avatar source={{
-            url : uriForImg
-          }} mr='8'/>
+          <Pressable onPress={()=>setShowModal(true)}>
+            <Avatar source={{
+              url : uriForImg
+            }} mr='8' />
+          </Pressable>
         )
 }}
 
-
-export default function(){
-
     return (
+      <>
         <Tab.Navigator>
         <Tab.Screen name="Tasks" component={TaskScreen} 
         options={options}/>
         <Tab.Screen name="Activity" component={ActivityScreen} options={options}/>
         <Tab.Screen name="Rewards" component={RewardScreen} options={options}/>
         </Tab.Navigator>
+
+        <Modal visible={showModal} presentationStyle="fullScreen" animationType="slide">
+          {editParent ?  <EditParentProfile showModal={()=>setShowModal(false)}/>
+          :
+          <EditChildProfile showModal={()=>setShowModal(false)}/>}
+           
+        </Modal>
+      </>
     )
 }
